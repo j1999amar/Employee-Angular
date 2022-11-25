9fd4cca133478456bdc05f4c7b6e63b96d0f6ccd
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-25 11:44:39 +0530

    Data Binding

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -6,9 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
-  name ="Amarnath";
-  designation="Traine";
-  salary=25000;
-  emailID="amarnath@nestgroup.net";
-  company="NeST Digital"
+data:any =  [{"name":"Amarnath",
+"designation":"Traine",
+"salary":25000,
+"emailID:":"amarnath@nestgroup.net",
+"company":"NeST Digital"},
+{"name":"Jude",
+  "designation":"Ceo",
+  "salary":50000,
+  "emailID:":"jude@nestgroup.net",
+  "company":"NeST Digital"},
+  {"name":"Sam",
+  "designation":"HR",
+  "salary":35000,
+  "emailID:":"amarnath@nestgroup.net",
+  "company":"NeST Digital"},
+  {"name":"StanLee",
+  "designation":"Manager",
+  "salary":75000,
+  "emailID:":"stanlee@nestgroup.net",
+  "company":"NeST Digital"},
+  {"name":"Hendry",
+  "designation":"Traine",
+  "salary":26000,
+  "emailID:":"henry@nestgroup.net",
+  "company":"NeST Digital"},
+  {"name":"Hellboy",
+  "designation":"Traine",
+  "salary":28000,
+  "emailID:":"hellboy@nestgroup.net",
+  "company":"NeST Digital"},
+  {"name":"Jennie",
+  "designation":"Traine",
+  "salary":24500,
+  "emailID:":"jennie@nestgroup.net",
+  "company":"NeST Digital"}]
 }
